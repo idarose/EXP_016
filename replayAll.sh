d269cb12e73ab8403e74aea969c73bd38b3de5ab
idarose <idarosenqvist99@gmail.com>
2025-05-06 14:33:17 +0200

    Conf

---
+++ replayAll.sh
@@ -0,0 +1,35 @@
+# source MakeFolder 262
+cd run_0262
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
+
+# source MakeFolder 263
+cd run_0263
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
+
+# source MakeFolder 264
+cd run_0264
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
+
+# source MakeFolder 265
+cd run_0265
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
+
+# source MakeFolder 266
+cd run_0266
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
+
+# source MakeFolder 267
+cd run_0267
+./gen_conf.py
+femul Topology_FromPSAToTreePRISMA.conf
+cd ..
